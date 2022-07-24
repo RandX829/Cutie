1edbb6e116341ca584614ea2792a7f2fcd9b9110
RandX <010and1001@gmail.com>
2022-07-24 20:06:08 +0900

    Set up

---
+++ Cutie-iOS/Cutie/ContentView.swift
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2022 RandX <010and1001@gmail.com>
+ *
+ * This file is part of Cutie.
+ *
+ * Cutie is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Cutie is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with Cutie.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+import SwiftUI
+import CutieCore
+
+struct ContentView: View {
+    let hello = HelloWorld().hello()
+    
+    var body: some View {
+        Text(hello)
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# Cutie(Working In Progress...)
+
+## What is Cutie?
+
+Cutie is A Personal Assistant.
+Aimed to release you from tedious tasks.
+
+## Develop Environment
+
+- MBP
+  - Intel
+  - 16GB
+- [Android Studio](https://developer.android.com/studio)
+- [Git](https://git-scm.com/)
+
+## Features
+
+(Working In Progress...)
+
+## Technical Stacks
+
+### Multiplatform
+
+- [Kotlin Multiplatform Mobile](https://kotlinlang.org/lp/mobile/)
+
+### Android
+
+- [Gradle](https://gradle.org/)
+- [Kotlin](https://kotlinlang.org/)
+
+### iOS
+- [CocoaPods](https://cocoapods.org/)
+- [Swift](https://www.swift.org/)
+
+### Other
+- [Git](https://git-scm.com/)
+- [EditorConfig](https://editorconfig.org/)
